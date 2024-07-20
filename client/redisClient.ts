62eb389014f4a0dd0bbc1aa78709c7942d56276e
HalkatHaathi <75882515+Barnamoy-roy@users.noreply.github.com>
2024-07-21 00:40:34 +0530

    added them to env

--- client/redisClient.ts
+++ client/redisClient.ts
@@ -1,8 +1,8 @@
 import { Redis } from '@upstash/redis'
 
 const redis = new Redis({
-  url: 'https://national-bluejay-44790.upstash.io',
-  token: 'Aa72AAIncDFlNmY5MTFlZGJjMTc0NDNiYWY0ZmE0ZjM5MTBiOTJhY3AxNDQ3OTA',
+  url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
 })
 
 export default redis;
